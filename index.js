44a78a2e85f4a116b61b27d8a221988ab4087399
RubenGH88 <104738166+RubenGH88@users.noreply.github.com>
2022-06-23 16:34:24 +0200

    mongoose lab
    only last bonus missing

--- index.js
+++ index.js
@@ -16,8 +16,49 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+			title: 'Fish finger and custard',
+			level: 'UltraPro Chef',
+			ingredients: [
+				'14-oz piece skinned and boned cod or haddock fillet',
+				'2 3/4 tbsp all-purpose flour',
+				'1 medium egg, beaten',
+				'1 1/4 cups fresh breadcrumbs oil, for shallow-frying',
+				'1 tbsp butter',
+				'1 1/4 cups milk',
+				'1 tsp Dijon mustard'
+			],
+			cuisine: 'Whovian',
+			dishType: 'dessert',
+			image: 'https://images.squarespace-cdn.com/content/v1/559dc96be4b099333339097f/1436405239562-APCKJQLY5ELPFX43F43V/image-asset.jpeg?format=1000w',
+			duration: 10,
+			creator: 'The Doctor'
+		});
   })
+  .then((response) => {
+   
+    return Recipe.insertMany(data);
+  })
+  .then((recipes) => {
+    recipes.forEach((element) => {
+      console.log(element.title);
+    });
+  })
+  .then((response) => {
+   
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese'}, 
+      {  duration: 100}, 
+     
+    );
+  })
+  .then((response) => {
+    
+    return Recipe.deleteOne({ title: 'Carrot Cake'});
+  })
+
+
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
